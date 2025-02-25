a5fdf1da32c2cddcf4df530c73df89e3fd78ff26
Elvadinho <elvado.temgoua@gmail.com>
2025-02-25 08:03:07 +0100

    new changes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,19 +1,20 @@
-function updateCountdown(){
+function updateCountdown() {
     const eventDate = new Date("March 1, 2025 00:00:00").getTime();
     const now = new Date().getTime();
-    const timeleft = eventDate - now;
+    const timeLeft = eventDate - now;
 
-    if(timeleft <= 0){
-        document.getElementById("timer").innerHTML = "The event has started";
+    if (timeLeft <= 0) {
+        document.getElementById("timer").innerHTML = "The event has started!";
         return;
     }
 
-    const days = Math.floor(timeleft / (1000 *60 *60 *24));
-    const hours = Math.floor((timeleft / (1000 *60 *60 *24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeleft / (1000 *60 *60)) / (1000 * 60));
-    const seconds = Math.floor((timeleft % (1000 * 60))/1000);
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
     document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
+
 updateCountdown();
-setInterval(updateCountdown,1000);
+setInterval(updateCountdown, 1000);
